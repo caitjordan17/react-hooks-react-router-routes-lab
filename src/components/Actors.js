23a65a113cf6c22460c31d2c632e5a86d6fd73be
Caitlin Jordan <jordan.cait17@gmail.com>
2023-09-23 15:32:57 -0700

    completed lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const actorsList = actors.map((actor) => (
+    <div>
+      <h3>Name: {actor.name}</h3>
+      <p>Movies:</p>
+      <ul>
+        {actor.movies.map((movie) => (
+          <li>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsList}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+  const movieList = movies.map((movie) => (
+    <div>
+      <h3>Name: {movie.title}</h3>
+      <p>Time: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {movie.genres.map((genre) =>(
+          <li>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+    );
 }
 
 export default Movies;
